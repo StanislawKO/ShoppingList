6c01a13c006aaad3268af0a157083bb723335db0
Stanislav Kozhicev <printks@yandex.ru>
2022-08-11 21:54:31 +0300

    Creating ShopItemActivity.kt throw ShopItemViewHolder.kt creating ShopItemDiffCallback.kt

---
+++ app/src/main/java/com/codexample/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.codexample.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codexample.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
